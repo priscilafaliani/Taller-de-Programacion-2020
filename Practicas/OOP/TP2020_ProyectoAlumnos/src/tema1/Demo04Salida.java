bd1443a730381a378a21d520d69382e3e999b00b
priscilafaliani <priscilafaliani@gmail.com>
2020-10-04 12:57:47 -0300

    uwu

---
+++ Practicas/OOP/TP2020_ProyectoAlumnos/src/tema1/Demo04Salida.java
@@ -0,0 +1,24 @@
+/*
+ Muestra Hola Mundo en consola.
+
+ */
+
+package src.tema1;
+/**
+ *
+ * @author vsanz
+ */
+public class Demo04Salida {
+
+   public static void main(String[] args) {
+        System.out.print("Hola Mundo! "); 
+        System.out.println("Hola Mundo! "); 
+        System.out.println(1234);
+        System.out.println(true);
+        int año=2016;
+        System.out.println ("Hola Mundo " + año + "!");
+
+   }
+}
+
+
