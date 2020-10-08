8434fafe46577c50ccf19d0dab0fa3fb37488127
priscilafaliani <priscilafaliani@gmail.com>
2020-10-08 18:25:55 -0300

    aburridooo

--- Teorias/Clase 4 POO/EjercicioTeoria1.java
+++ Teorias/Clase 4 POO/EjercicioTeoria1.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package clase2;
-
-/**
- *
- * @author Victoria
- */
 public class Auto {
     private int patente;
     private String dueño;
