d87d9758b3134659fb4a6a39ebb3ed07be0e9ae4
priscilafaliani <priscilafaliani@gmail.com>
2020-10-10 21:02:43 -0300

    Update Triangulo.java

--- Practicas/OOP/Practica 2/src/Triangulo.java
+++ Practicas/OOP/Practica 2/src/Triangulo.java
@@ -74,7 +74,7 @@ public class Triangulo {
     // general methods
     public double calcularArea(){
         double s = (lado1 + lado2 + lado3) / 2;
-        return Math.sqrt(s * (s - lado1) * (s - lado2) + (s - lado3));
+        return Math.sqrt(Math.abs(s * (s - lado1) * (s - lado2) + (s - lado3)));
     }
 
     public double calcularPerimetro(){
